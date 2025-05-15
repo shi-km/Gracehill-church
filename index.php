786576a0642046cd1e291243416522828d256c0c
shi-km <julietwanjiru333@gmail.com>
2025-05-15 13:46:04 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gracehill church</title>
+
+    <!-- Bootstrap links  -->
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" 
+     integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+     <link rel="stylesheet" href="styles.css">
+  </head>
+</head>
+<body>
+
+<!-- Header section -->
+
+<nav class="navbar bg-light">
+  <div class="container-fluid">
+    <img src="./assets/images/Logo.png" alt="logo" class="img-fluid">
+        <a href="">Home</a>
+        <a href="">About Us</a>
+        <a href="">Connect</a>
+        <a href="">Resources</a>
+        <a href="">Ministries</a>
+        <a href="">Give</a>
+        <a href="">Contact Us</a>
+  </div>
+</nav>
+
+<!-- End of Header section -->
+
+<!-- Hero section -->
+<div class="container-fluid bg-light hero-section">
+    <div class="row p-3 position-relative">
+        <div class="col-md-4 d-flex align-items-center">
+         <h1 class="hero-title">All for The King and His Kingdom</h1>
+        </div>
+        <div class="col-md-8">
+          <img src="./assets/images/hero.png" alt="hero-image" class="img-fluid">
+        </div>
+    </div>
+</div>
+
+<!-- Welcome note section -->
+<div class="container-fluid my-5">
+  <div class="container-fluid p-3">
+  <div class="card w-100" style="background-color:#116A3A;">
+    <div class="row g-0">
+      <div class="col-md-4">
+        <img src="./assets/images/welcome.png" class="img-fluid h-100 w-100 object-fit-cover rounded-start" alt="Card Image">
+      </div>
+      <div class="col-md-8 d-flex align-items-center">
+        <div class="card-body" >
+          <h1 class="card-title text-center text-white">Welcome Note</h1>
+          <p class="card-text p-5 text-white lh-lg">
+            Welcome to GRACEHILL MISSION CHURCH, where God writes beautiful stories.  We are a Jesus-loving family that is passionate about his word and his kingdom.  We believe that God is shaping and writing our stories for his glory and his honor.  We believe that corporately and individually, we are pens in the hand of God—he is using us to write his story in his world as we obey his word and pursue his commands. We are a family and missions focused church.  We believe everyone matters and ought to be discipled soundly enough to grow into maturity in Christ and to thrive in their place of service.
+
+           Our vision—what God is building here—is to see a gospel movement that is a reflection of God’s image and a channel of his love to the world.  Our desire is to know more of Jesus, and express more of him in everything we do, everywhere we go.  We are glad to be part of the beautiful kingdom story that God is writing! Welcome and be part of the unfolding story!
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</div><!-- container-fluid -->
+
+<!-- welcome note section end -->
+
+
+<!-- worship section  -->
+
+
+
+    <!-- Bootstrap js -->
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</body>
+</html>
